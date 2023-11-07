4c7861d5791e2148d54fa3f1985d0a7d5eb403e7
mh.termzee <m.termzee@basecom.com>
2023-11-07 10:40:51 +0100

    creating the Detail Page

--- pages/currency/[id].vue
+++ pages/currency/[id].vue
@@ -1,3 +1,26 @@
-<template></template>
+<template>
+	<div>
+		<h2>{{ coin.name }} Detail page</h2>
+		<table border="1 px solid">
+			<thead>
+				<th>Symbol</th>
+				<th>Rank</th>
+				<th>Price</th>
+				<th>Market Caü - US $</th>
+			</thead>
+			<tr>
+				<td>{{ coin.symbol }}</td>
+				<td>{{ coin.rank }}</td>
+				<td>{{ coin.price_usd }}</td>
+				<td>{{ coin.market_cap_usd }}</td>
+			</tr>
+		</table>
+	</div>
+</template>
 
-<script setup></script>
+<script setup>
+const route = useRoute();
+const id = route.params.id;
+const { data } = await useFetch("/api/ticker/?id=" + id);
+const coin = data.value[0];
+</script>
